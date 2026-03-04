e628c1c4d008f65f9eb767a6fb602df02b1d291f
danroozemond <d.roozemond@tibo.energy>
2026-03-04 19:21:10 +0100

    adjust info text with some actual content

--- src/components/Infotext.tsx
+++ src/components/Infotext.tsx
@@ -1,8 +1,11 @@
 function Infotext() {
   return (
     <p className="mb-0">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua.
+      Hierboven vind je de stromingsdata van de geselecteerde duikstek.
+        Je ziet een lijn bij 20 cm/s, wat algemeen geaccepteerd is als de grens
+        voor een comfortabele duik.
+
+        Kijk voor meer informatie over getijdenduiken plannen op <a href="https://onderwatersport.org/voor-duikers/getijdentabel-en-check-de-stek/">de site van de NOB</a>.
     </p>
   )
 }
