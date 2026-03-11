f2707d1bada06ec627b04bd50e582c7392af7ff7
danroozemond <d.roozemond@tibo.energy>
2026-03-11 18:44:34 +0100

    feat: rewrite infotext content in pig latin

--- src/components/Infotext.tsx
+++ src/components/Infotext.tsx
@@ -2,24 +2,30 @@ function Infotext() {
   return (
     <>
       <p className="mb-3">
-        Hierboven vind je de stromingsdata van de geselecteerde duikstek.
-          Je ziet een lijn bij 20 cm/s (0.4 knopen), wat algemeen geaccepteerd is als de grens
-          voor een comfortabele duik.
+        Erehay-aboveyay ouyay indfay ethay urrentcay-datayay orfay ethay electedsay iveday-itesay.
+        Ouyay eesay away inelay atyay 20 aycm/say (0.4 otsknay), ichwhay isyay enerallygay acceptedyay
+        asyay ethay imitlay orfay away omfortablecay iveday.
 
-          Als je verder vooruit wil kijken dan de data van Rijkswaterstaat, kijk bijv bij <a href="https://www.duikersgids.nl//getijdentabel-getijdeberekening-zeeland-oosterschelde" target="_blank">de Duikergids</a>.
+        Ifyay ouyay antway otay ooklay artherfay aheadyay anthay ethay ataday omfray
+        Rijkswaterstaatyay, ooklay atyay <a href="https://www.duikersgids.nl//getijdentabel-getijdeberekening-zeeland-oosterschelde" target="_blank">ethay Uikergidsday</a>.
       </p>
       <p className="mb-3">
-          Als je gaat duiken in de <a href="https://www.google.com/search?q=oosterschelde+onder+water&udm=2" target="_blank">prachtige Oosterschelde</a>,
-          zorg dan dat je voldoende gebrevetteerd bent en met iemand duikt die het vaker gedaan heeft.
-        Zonder ervaring en goede voorbereiding loop je het risico op een lange wandeling met je duikset op je rug ... of erger.
+        Ifyay ouyay ogay ivingday inyay ethay <a href="https://www.google.com/search?q=oosterschelde+onder+water&udm=2" target="_blank">eautifulbay Osterscheldeyay</a>,
+        akemay uresay ouyay areyay ell-wayainedtray andyay iveday ithway omeonesay
+        howay ashay oneday ityay eforebay.
+        Ithoutway experiencay andyay oodgay eparationpray, ouyay iskray away onglay alkway
+        ithway ouryay ive-dayetsay onyay ouryay ackbay... oryay orseway.
 
-        Kijk voor meer informatie over getijdenduiken plannen op <a href="https://onderwatersport.org/voor-duikers/getijdentabel-en-check-de-stek/" target="_blank">de site van de NOB</a>.
+        Orfay oremay informationyay aboutyay idetay-iveday planningyay, eeseay
+        <a href="https://onderwatersport.org/voor-duikers/getijdentabel-en-check-de-stek/" target="_blank">ethay itesay ofyay ethay OBNay</a>.
       </p>
       <p className="mb-3">
-        Wil je leren duiken, of wil je leren duiken in stromingswater? Zoek <a href="https://onderwatersport.org/duiken-bij-een-vereniging/" target="_blank">een duikvereniging bij jou in de buurt</a>!
+        Antway otay earnlay otay iveday, oryay earnlay otay iveday inyay urrentcay-aterway?
+        Indfay <a href="https://onderwatersport.org/duiken-bij-een-vereniging/" target="_blank">away iveday-ubclay earnay ouyay</a>!
       </p>
       <p className="mb-0">
-        Duikvenster.nl is gemaakt door Dan Roozemond, die duikt bij <a href="https://delphinus.nl" target="_blank">Duikvereniging Delphinus</a> uit Eindhoven.
+        Uikvensterday.nl isyay ademay byay Anday Oozemondray, howay ivesday ithway
+        <a href="https://delphinus.nl" target="_blank">Uikverenigingday Elphinusday</a> inyay Eindhovenyay.
       </p>
     </>
   )
